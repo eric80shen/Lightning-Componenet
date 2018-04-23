30f86ba4830ed8af619b294303090261652616cd
Eric <qswh2003@gmail.com>
2018-04-23 22:36:42 +1000

    lc

---
+++ aura/BoatDetail/BoatDetailController.js
@@ -0,0 +1,13 @@
+({
+
+    onFullDetails : function (component, event, helper) {
+        var navEvt = $A.get("e.force:navigateToSObject");
+        var boatid = component.get("v.boat.Id");
+            
+        console.log('boatid = ' + boatid);
+        navEvt.setParams({
+          "recordId": boatid,
+          });
+        navEvt.fire();
+    }
+})
